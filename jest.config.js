38624f09083debf3f028b0a93fad716c87b22ca0
Sviatlana Tumilevich <sviatlana.tumilevich@epam.com>
2023-04-28 18:34:45 +0300

    add Jest with Playwright configuration

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable linebreak-style */
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+
+	clearMocks: true,
+
+	// A list of reporter names that Jest uses when writing coverage reports
+	coverageReporters: [
+		`text`
+	],
+
+	// The root directory that Jest should scan for tests and modules within
+	rootDir: `./tests.jest`,
+
+	// The number of seconds after which a test is considered as slow and reported as such in the results.
+	slowTestThreshold: 5,
+
+	// The glob patterns Jest uses to detect test files
+	testMatch: [
+		`tests.jest/**/*.[jt]s?(x)`,
+		`**/?(*.)+(spec|test).[tj]s?(x)`
+	],
+
+	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+	testPathIgnorePatterns: [
+		`\\\\node_modules\\\\`
+	],
+
+	preset: `jest-playwright-preset`
+};
